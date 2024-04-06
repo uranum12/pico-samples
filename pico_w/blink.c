e1601b223de372528d84afb3d6a1491eb31559a3
uranum12 <113694643+uranum12@users.noreply.github.com>
2024-04-06 14:45:00 +0900

    add raspberry pi pico w blink sample

---
+++ pico_w/blink.c
@@ -0,0 +1,16 @@
+#include "pico/cyw43_arch.h"
+#include "pico/stdlib.h"
+
+int main() {
+    stdio_init_all();
+    if (cyw43_arch_init()) {
+        printf("Wi-Fi init failed");
+        return 1;
+    }
+    for (;;) {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        sleep_ms(500);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        sleep_ms(500);
+    }
+}
